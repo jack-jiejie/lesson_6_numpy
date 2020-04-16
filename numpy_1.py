47e3f92c7d3d69a6c7c45295a5d285ec73e853e2
yin <jack@yindeMacBook-Pro.local>
2020-04-16 17:40:10 +0800

    numpy and pandas学习

---
+++ numpy_1.py
@@ -0,0 +1,30 @@
+__author__ = "yzj"
+__data__ = "2020/4/15 13:39"
+
+import numpy as np
+
+# data = [1, 2, 3, 4, 5, 6]
+# print(data)
+# n = np.array(data)
+# print(n)
+# print(n.shape)
+# print(n.dtype)
+# n1 = np.empty((2, 3))
+# print(n1)
+# for i in np.arange(10):
+#     print(i)
+# 类型转换
+# arr = np.array([1.2, 2.1, 3.1, 4.2, -2.1, -2.2, -3.3])
+# print(arr)
+# print(arr.astype(np.int32))
+# 矢量化
+"""
+进行数据的批量运算和处理， 就叫矢量化(在不写循环的情况下)
+对应位置相加
+"""
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([12, 13, 52, 15])
+# print(arr1 + arr2)
+# print(arr1 * arr2)
+# 索引和切片
+
